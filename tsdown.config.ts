f6eec98d3ca547920946dcc221cf13735d8964b9
thejackshelton <me@jackshelton.com>
2025-11-29 16:28:52 -0600

    feat: updated bundler config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from "tsdown";
+import pkg from "./package.json" with { type: "json" };
+
+const { dependencies = {}, peerDependencies = {} } = pkg as {
+	dependencies?: Record<string, string>;
+	peerDependencies?: Record<string, string>;
+};
+
+const makeRegex = (dep: string) => new RegExp(`^${dep}(/.*)?$`);
+const excludeAll = (obj: Record<string, string>) =>
+	Object.keys(obj).map(makeRegex);
 
 export default defineConfig({
 	entry: ["./src/index.ts"],
 	platform: "neutral",
-	external: ["node:module", "node:fs", "node:child_process"],
+	external: [
+		/^node:.*/,
+		...excludeAll(dependencies),
+		...excludeAll(peerDependencies),
+	],
 });
